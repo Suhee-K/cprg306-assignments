4266682df0e53e34855d7aee5836d7f5c1c585aa
Suhee Kim <tngml118@gmail.com>
2024-09-09 11:34:07 -0600

    finish week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+        <p>Suhee Kim</p>
+        <Link href="https://github.com/Suhee-K?tab=repositories">Go to my GitHub repository</Link>
+    </div>
+  )
+}
